4c86c59ebdac37458aece90fd6cf73710158f3eb
chunchunmaru0 <aalupidalu10@gmail.com>
2023-05-09 02:21:19 -0700

    fixed example to the new one

--- test/script2.mjs
+++ test/script2.mjs
@@ -1,10 +1,19 @@
 import ksk from "ksk-metadata";
 
-let urlArr = ['https://ksk.moe/view/9/3633ca4815bd','https://ksk.moe/view/11467/b1a36dc6d714','https://ksk.moe/view/11463/0617459162d3','https://ksk.moe/view/11457/abcb247997b2']
-const kskRes = (await ksk(urlArr,3000))
+let urlArr = [
+    'https://ksk.moe/view/9/3633ca4815bd',
+    'https://ksk.moe/view/11457/abcb247997b2',
+]
+
+
+const kskRes = (await ksk(urlArr, 3000)) //second param is Rate Limit in ms, change it to your liking
 
 console.log(kskRes)
 
-//do something with the result 
+/*____________________OR_____________________*/
+let kskResult = (await ksk('https://ksk.moe/view/11457/abcb247997b2')) //can just be single url
 
-//console.log(JSON.stringify(kskRes)) //jsonify the output obj to jsonObj
+
+//do something with the result
+
+console.log(JSON.stringify(kskRes)) //jsonify the output obj to jsonObj
