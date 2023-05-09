4c02fa02b1ad7a5224d884beeeaefd0fc363a8b7
chunchunmaru0 <aalupidalu10@gmail.com>
2023-05-09 02:14:19 -0700

    Function to scrape the md

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const Crawler = require('crawler')
+const Crawler = require('crawler');
+const getMetaData = require('./getMD');
 var metaData;
 
 
 
 async function ksk(kskUrls,rate) {
-    if(rate == undefined){ //rate must be a number
+    if(rate === undefined){ //rate must be a number
         rate =0;
     }
     return new Promise((resolve, reject) => {
@@ -41,149 +42,5 @@ async function ksk(kskUrls,rate) {
       });
 }
 
-function getMetaData(res){
-    {
-        const $ = res.$;
-        //console.dir(res.$('#metadata').html());
-        console.log("Scraping: ",res.request.uri.href)
-        let title,fullTitle;
-        let tags = [];
-        let category, parody, magazine, toalPages, oSize, rSize, uploadedAt, archivedAt, publishedAt, updatedAt
-        let authors = [];
-        let circle = [];
-        let links=[{label:'ksk.moe', url: res.request.uri.href}]
-        
-        let extLink, label;
-        //console.log($('#metadata').find('main >div>div >a').html())
-        let thumbnail = {
-            url :($('#cover').find('img').attr('src')),
-            title: ($('#cover').find('img').attr('title')),
-            alt: ($('#cover').find('img').attr('alt')),
-        }
-        title =   $('#metadata').find('header >h1').text()
-        fullTitle =$('#metadata').find('header >h2').text()
 
-        if(title == ''){ metaData = { 'sucess': false }; console.log('Something went wrong! Most Probably you entered wrong URL! Error Url: ', res.request.uri.href); return(metaData); }
-        //console.log(title,fullTitle)
-        //console.log(thumbnail)
-        $('#metadata').find('main >div>strong').each(function (index, element) {
-
-            switch ($(element).text()) {
-                case 'Category':
-                    $(element).parent().find('div >a').each(function (ind, el) {
-                        category = $(el).text().replace(/\n/g, '')
-                    })
-                    break;
-                case 'Artist':
-                    $(element).parent().find('div >a').each(function (ind, el) {
-                        let junkAuthors = $(el).text().replace(/\n/g, '')
-                        authors.push(junkAuthors.replace(/\d+/g, ''))
-                    })
-                    break;
-                case 'Circle':
-                    $(element).parent().find('div >a').each(function (ind, el) {
-                        let junkCircle = $(el).text().replace(/\n/g, '')
-                        circle.push(junkCircle.replace(/\d+/g, ''))
-                    })
-                    break;
-                case 'Parody':
-                        parody = $(element).parent().find('span').html();
-                    break;
-                case 'Magazine':
-                    
-                        magazine = $(element).parent().find('span').html()
-                        //magazine = junkMagazine.replace(/\d+/g, '')
-                        //console.log(magazine)
-
-                    break;
-                case 'Tag':
-                    $(element).parent().find('div >a').each(function (ind, el) {
-                        //console.log("Tags:", )
-                        let junkTags = ($(el).text())
-                        junkTags = junkTags.replace(/\n/g, '')
-                        let tag = junkTags.replace(/\d.+/g, '')
-                        tags.push(tag)
-                    })
-                    break;
-                case 'Length':
-                    $(element).parent().find('div >a').each(function (ind, el) {
-                        toalPages = ($(el).text()).replace(/\n/g, '')
-                    })
-                    break;
-                case 'Metadata':
-                    $(element).parent().find('div >a').each(function (ind, el) {
-                        //console.log($(el).attr('href'))
-                        label = $(el).text(),
-                        extLink = `https://ksk.moe${$(el).attr('href')}`
-                        links.push({
-                            label: label,
-                            url: extLink
-                        })
-
-                    })
-                    break;
-                case 'Size (Ori.)':
-                    oSize = $(element).parent().find('div >div >span').html();
-                    break;
-                case 'Size (Res.)':
-                    rSize = $(element).parent().find('div >div >span').html();
-                    break;
-                case 'Uploaded':
-                    uploadedAt = $(element).parent().find('div >time').attr('data-timestamp')
-                    break;
-                case 'Archived':
-                    archivedAt = $(element).parent().find('div >time').attr('data-timestamp')
-                    break;
-                case 'Published':
-                    publishedAt = $(element).parent().find('div >time').attr('data-timestamp')
-                    break;
-                case 'Updated':
-                    updatedAt = $(element).parent().find('div >time').attr('data-timestamp')
-                    break;
-                default:
-                    break;
-            }
-
-
-        })
-
-        metaData = {
-            "sucess": true,
-            "title": title,
-            'full_title': fullTitle,
-            'thumbnail': thumbnail,
-            "category":category,
-            "authors": authors,
-            "parody":parody,                       
-            "page":toalPages,
-            "original_size":oSize,
-            "resampled_size":rSize,                    
-            "tags": tags,
-            'publisher': label,
-            "links": links,
-
-
-        }
-        if(circle.length != 0){
-            metaData['circle'] = circle
-        }
-        
-        if(magazine != undefined){
-            metaData['magazine'] = magazine
-        }
-        dateMd ={
-            "uploaded_date": uploadedAt,
-            "archived_date":archivedAt,                        
-            "published_date": publishedAt,
-            "updated_date":updatedAt,
-        }
-        //adding dates to the last
-        Object.entries(dateMd).forEach(([k,v]) => {metaData[k] = v})
-        //console.log(metaData)
-        //console.log(magazine,circle)
-        
-        return (metaData);
-
-    }
-}
 module.exports = ksk;
--- test/script2.mjs
+++ test/script2.mjs
@@ -1,8 +1,10 @@
 import ksk from "ksk-metadata";
 
-const kskRes = (await ksk('https://ksk.moe/view/11463/0617459162d3'))
+let urlArr = ['https://ksk.moe/view/9/3633ca4815bd','https://ksk.moe/view/11467/b1a36dc6d714','https://ksk.moe/view/11463/0617459162d3','https://ksk.moe/view/11457/abcb247997b2']
+const kskRes = (await ksk(urlArr,3000))
 
 console.log(kskRes)
+
 //do something with the result 
 
-console.log(JSON.stringify(kskRes)) //jsonify the output obj to jsonObj
+//console.log(JSON.stringify(kskRes)) //jsonify the output obj to jsonObj
